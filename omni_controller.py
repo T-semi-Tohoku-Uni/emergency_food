2b4cf44caf2e68dd528503a5dfe995de08f0d219
Bacillus-Subtilis <subtiilis27@gmail.com>
2026-05-20 02:07:52 +0900

    omni_controller written

--- omni_controller.py
+++ omni_controller.py
@@ -1,5 +1,6 @@
 import time
 from i2c_controller import ServoController
+import math
 
 # 方針
 # その場回転とベクトルとxy方向の速度指定のコードを実装する。
@@ -11,8 +12,50 @@ class OmuniSpeed:
         self.front_right = front_right
         self.rear_left = rear_left
         self.rear_right = rear_right
+        # サーボコントローラーの初期化
+        rot_servo = ServoController()
 
+        # 前方をy,右向きをxと億
     def Speedxy(self,x,y):
+        norm = 0.7
+        nx = norm*x
+        ny = norm*y
+
+        v_a = nx - ny
+        v_b = nx + ny
+        v_c = -nx + ny
+        v_d = -nx - ny
+        
+        rot_servo.set_speed(self.front_left, v_a)
+        rot_servo.set_speed(self.front_right, v_b)
+        rot_servo.set_speed(self.rear_left, v_c)
+        rot_servo.set_speed(self.rear_right, v_d)        
 
 
-    def SpeedPolar(self,x,y):
+    #前方向からの角度をphiとおく、
+    def SpeedPolar(self,v,phi):
+        root2^-1 = 0.707
+        nx = v * root2^-1 * math.sin(math.radians(phi))
+        ny = v * root2^-1 * math.cos(math.radians(phi))
+
+        v_a = nx - ny
+        v_b = nx + ny
+        v_c = -nx + ny
+        v_d = -nx - ny
+        
+        rot_servo.set_speed(self.front_left, v_a)
+        rot_servo.set_speed(self.front_right, v_b)
+        rot_servo.set_speed(self.rear_left, v_c)
+        rot_servo.set_speed(self.rear_right, v_d) 
+
+    def rotation(self,omega):
+        norm = 1
+        v_a = norm * omega
+        v_b = norm * omega
+        v_c = norm * omega
+        v_d = norm * omega
+
+        rot_servo.set_speed(self.front_left, v_a)
+        rot_servo.set_speed(self.front_right, v_b)
+        rot_servo.set_speed(self.rear_left, v_c)
+        rot_servo.set_speed(self.rear_right, v_d) 
