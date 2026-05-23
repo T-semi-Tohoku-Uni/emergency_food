7f62154167e8b0986a9f1f414e4f8b589df3f39d
Amewasa <maker@amewasa.com>
2026-05-23 11:16:06 +0900

    change class name

--- omni_controller.py
+++ omni_controller.py
@@ -5,7 +5,7 @@ import math
 # 方針
 # その場回転とベクトルとxy方向の速度指定のコードを実装する。
 
-class OmuniSpeed:
+class OmuniDrive:
     def __init__(self,front_left=0,front_right=1,rear_left=2,rear_right=3,max_speed = 1.0):
         #speedxy用の変数
         self.front_left= front_left
